a427e9d8504ca456e59c8c805c958609d21ed17e
VitorScheffler <vitor.scheffler@hotmail.com>
2025-12-04 14:22:03 +0000

    correção do botão

--- pages/entrada.php
+++ pages/entrada.php
@@ -145,7 +145,7 @@ ob_start();
 <?php endif; ?>
 
 <div class="row">
-    <div class="col-lg-6">
+    <div class="col-lg-12">
         <div class="card mb-4">
             <div class="card-header bg-primary text-white">
                 <h5 class="mb-0">
@@ -281,7 +281,7 @@ ob_start();
         </div>
     </div>
     
-    <div class="col-lg-6">
+    <div class="col-lg-12">
         <div class="card">
             <div class="card-header bg-light">
                 <div class="d-flex justify-content-between align-items-center">
